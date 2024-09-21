fceb1ab6001a684d9196127b050996f4d5c734f6
JoanRey19 <166669894+JoanRey19@users.noreply.github.com>
2024-09-21 13:37:23 -0400

    Create MiembroDeLaComunidad.cs
    Añadida clase MiembroDeLaComunidad con método virtual MostrarDatos()

---
+++ src/POOconcepts/InstitutoAcademico/Entidades/MiembroDeLaComunidad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstitutoAcademico.Entidades
+{
+    public class MiembroDeLaComunidad : Instituto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Id { get; set; }
+        public string Correo { get; set; }
+
+        public virtual void MostrarDatos()
+        {
+            base.MostrarInstituto();
+            Console.WriteLine($"Nombre: {Nombre} {Apellido}, ID: {Id}");
+            Console.WriteLine($"Correo: {Correo}");
+           
+        }
+
+    }
+}
