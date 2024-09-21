b7f01a69651580b9703fdd689595cacef11cc013
JoanRey19 <166669894+JoanRey19@users.noreply.github.com>
2024-09-21 13:34:11 -0400

    Create Instituto.cs
    Se agrega la clase Instituto con sus atributos básicos (NombreInstituto, Direccion, Telefono, Fundacion) y el método MostrarInstituto() para mostrar la información del instituto.

---
+++ src/POOconcepts/InstitutoAcademico/Entidades/Instituto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstitutoAcademico.Entidades
+{
+    public class Instituto
+    {
+        public string NombreInstituto {  get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int Fundacion { get; set; }
+
+        public void MostrarInstituto()
+        {
+            Console.WriteLine($"Instituto: {NombreInstituto}");
+            Console.WriteLine($"Dirección: {Direccion}");
+            Console.WriteLine($"Teléfono: {Telefono}");
+            Console.WriteLine($"Año de Fundación: {Fundacion}");
+        }
+    }
+}
